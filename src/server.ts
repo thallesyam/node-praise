53cad586262998771415087ba342a7cd22fd409c
Thalles Ian <thallesyam@users.=.github.com>
2022-01-19 15:45:25 -0300

    feat: create test routes

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,14 @@ import express from 'express'
 
 const app = express()
 
+app.get('/test', (request, response) => {
+  return response.send('Olá Nlw')
+})
+
+app.post('/test-post', (request, response) => {
+  return response.send('Nlw método POST')
+})
+
 app.listen(3333, () => {
   console.log('Server is Running 🔥')
 })
