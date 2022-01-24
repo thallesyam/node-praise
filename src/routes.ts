03b0b73c662fe7e32d2e31746ee15d51a8c6c393
Thalles Ian <thallesyam@users.=.github.com>
2022-01-24 17:55:42 -0300

    feat: create authentication and compliments table

--- src/routes.ts
+++ src/routes.ts
@@ -3,14 +3,20 @@ import { ensureAdmin } from './middlewares/ensureAdmin'
 
 import { CreateTagController } from './controllers/CreateTagController'
 import { CreateUserController } from './controllers/CreateUserController'
+import { AuthenticateUserController } from './controllers/AuthenticateUserController'
+import { CreateComplimentController } from './controllers/CreateComplimentController'
 
 const router = Router()
 
 // Controllers
 const createUserController = new CreateUserController()
 const createTagController = new CreateTagController()
+const authenticateUserController = new AuthenticateUserController()
+const createComplimentController = new CreateComplimentController()
 
 router.post('/users', createUserController.handle)
 router.post('/tags', ensureAdmin, createTagController.handle)
+router.post('/login', authenticateUserController.handle)
+router.post('/compliments', createComplimentController.handle)
 
 export { router }
