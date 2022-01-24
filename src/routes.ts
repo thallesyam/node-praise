5204acbbd76e0c8773b0c0b8761945121a26ab11
Thalles Ian <thallesyam@users.=.github.com>
2022-01-24 11:11:26 -0300

    feat: create ensureAdmin middleware

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,16 @@
 import { Router } from 'express'
+import { ensureAdmin } from './middlewares/ensureAdmin'
+
 import { CreateTagController } from './controllers/CreateTagController'
 import { CreateUserController } from './controllers/CreateUserController'
 
 const router = Router()
 
 // Controllers
-
 const createUserController = new CreateUserController()
 const createTagController = new CreateTagController()
 
 router.post('/users', createUserController.handle)
-router.post('/tags', createTagController.handle)
+router.post('/tags', ensureAdmin, createTagController.handle)
 
 export { router }
