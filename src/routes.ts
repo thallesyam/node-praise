b398ee0c527c3cbd0f6cb892a11d204eecedead0
Thalles Ian <thallesyam@users.=.github.com>
2022-01-26 17:10:19 -0300

    feat: create users, tags and compliments list

--- src/routes.ts
+++ src/routes.ts
@@ -6,14 +6,26 @@ import { CreateTagController } from './controllers/CreateTagController'
 import { CreateUserController } from './controllers/CreateUserController'
 import { AuthenticateUserController } from './controllers/AuthenticateUserController'
 import { CreateComplimentController } from './controllers/CreateComplimentController'
+import { ListUserSendComplimentsController } from './controllers/ListUserSendComplimentsController'
+import { ListUserReceiverComplimentsController } from './controllers/ListUserReceiverComplimentsController'
+import { ListTagsController } from './controllers/ListTagsController'
+import { ListUsersController } from './controllers/ListUsersController'
 
 const router = Router()
 
 // Controllers
+const listUsersController = new ListUsersController()
 const createUserController = new CreateUserController()
+const listTagsController = new ListTagsController()
 const createTagController = new CreateTagController()
 const authenticateUserController = new AuthenticateUserController()
 const createComplimentController = new CreateComplimentController()
+const listUserSendComplimentsController =
+  new ListUserSendComplimentsController()
+const listUserReceiveromplimentsController =
+  new ListUserReceiverComplimentsController()
+
+// Routes
 
 router.post(
   '/tags',
@@ -28,5 +40,18 @@ router.post(
   ensureAuthenticated,
   createComplimentController.handle
 )
+router.get(
+  '/users/compliments/send',
+  ensureAuthenticated,
+  listUserSendComplimentsController.handle
+)
+router.get(
+  '/users/compliments/receive',
+  ensureAuthenticated,
+  listUserReceiveromplimentsController.handle
+)
+
+router.get('/tags', ensureAuthenticated, listTagsController.handle)
+router.get('/users', ensureAuthenticated, listUsersController.handle)
 
 export { router }
